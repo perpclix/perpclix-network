211eb1a1e8355eaf1f7c7dd88665bcec0ba82e49
perpclix <cikurkelvin472@gmail.com>
2026-07-04 15:20:37 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import MarketList from "@/components/MarketList";
+import TradingTerminal from "@/components/TradingTerminal";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <MarketList />
+      <TradingTerminal />
+    </>
+  );
+}
